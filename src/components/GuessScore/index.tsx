b3def6d12997d64e959f06f07d8f419ec864dfe7
Adrien Guéret <adrien.grt@gmail.com>
2025-06-18 13:05:11 +0200

    feat(guess-score): move it at bottom left

--- src/components/GuessScore/index.tsx
+++ src/components/GuessScore/index.tsx
@@ -5,22 +5,12 @@ import "./GuessScore.css";
 type Props = {
   distance: number;
   score: number;
-  x1: number;
-  y1: number;
-  x2: number;
-  y2: number;
 };
 
-export default function GuessScore({ distance, score, x1, y1, x2, y2 }: Props) {
+export default function GuessScore({ distance, score }: Props) {
   const { translate } = useTranslations();
   return (
-    <div
-      className="game-guess-score"
-      style={{
-        left: `${(x1 + x2) / 2 - 20}px`,
-        top: `${(y1 + y2) / 2 - 20}px`,
-      }}
-    >
+    <div className="game-guess-score">
       <div>
         <label>{translate("distance.label")}</label>
         <span>{translate("distance.value")(distance)}</span>
