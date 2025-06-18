fea47ead065dc409b235f9bcb0324376996e9456
Adrien Guéret <adrien.grt@gmail.com>
2025-06-18 09:19:09 +0200

    feat(language): update design of LanguageSelector

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,10 +5,11 @@ import "./Button.css";
 
 export default function Button({
   children,
+  className = "",
   ...otherProps
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button className="game-button" {...otherProps}>
+    <button className={`game-button ${className}`} {...otherProps}>
       {children}
       <img draggable="false" alt="" src={arrowUrl} />
     </button>
