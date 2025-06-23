183645036fbefd07cc5109c097058763387a92df
Adrien Guéret <adrien.grt@gmail.com>
2025-06-23 21:36:11 +0200

    feat(guess-score): move 'guess score' info

--- src/components/GuessScore/index.tsx
+++ src/components/GuessScore/index.tsx
@@ -11,13 +11,15 @@ export default function GuessScore({ distance, score }: Props) {
   const { translate } = useTranslations();
   return (
     <div className="game-guess-score">
-      <div>
-        <label>{translate("distance.label")}</label>
-        <span>{translate("distance.value")(distance)}</span>
-      </div>
-      <div>
-        <label>{translate("score.label")}</label>
-        <span>{translate("score.value")(score)}</span>
+      <div className="game-guess-score-inner">
+        <div>
+          <label>{translate("distance.label")}</label>
+          <span>{translate("distance.value")(distance)}</span>
+        </div>
+        <div>
+          <label>{translate("score.label")}</label>
+          <span>{translate("score.value")(score)}</span>
+        </div>
       </div>
     </div>
   );
