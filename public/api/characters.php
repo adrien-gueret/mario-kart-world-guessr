23174c6ded87a01413d25b740bce82d75ff44b60
Adrien Guéret <adrien.grt@gmail.com>
2025-08-06 23:35:05 +0200

    feat(characters): display clue on how to unlock characters

--- public/api/characters.php
+++ public/api/characters.php
@@ -14,7 +14,7 @@ if (empty($currentUser)) {
 
 try {
     $stmt = $pdo->prepare(
-        "SELECT c.id,
+        "SELECT c.id, a.clue_fr, a.clue_en,
         CASE
             WHEN c.id_achievement IS NULL THEN TRUE
             WHEN ua.id_achievement IS NOT NULL THEN TRUE
@@ -22,7 +22,9 @@ try {
         END AS is_unlocked
         FROM `mario-kart-world-characters` c
         LEFT JOIN `mario-kart-world-users-unlocked-achievements` ua
-        ON c.id_achievement = ua.id_achievement AND ua.id_user = :userId");
+        ON c.id_achievement = ua.id_achievement AND ua.id_user = :userId
+        LEFT JOIN `mario-kart-world-achievements` a ON a.id = c.id_achievement
+      ");
         
     $stmt->bindValue(':userId', $currentUser['id'], PDO::PARAM_INT);
 
@@ -33,9 +35,14 @@ try {
     $characters = [];
 
     foreach($fetchedCharacters as $fetchedCharacter) {
-        if ($fetchedCharacter['is_unlocked'] === 1) {
-            $characters[] = $fetchedCharacter['id'];
-        }
+        $characters[] = [
+            'id' => $fetchedCharacter['id'],
+            'unlockClue' => [
+                'fr' => $fetchedCharacter['clue_fr'],
+                'en' => $fetchedCharacter['clue_en'],
+            ],
+            'isUnlocked' => $fetchedCharacter['is_unlocked'] === 1,
+        ];    
     }
   
     echo json_encode($characters);
