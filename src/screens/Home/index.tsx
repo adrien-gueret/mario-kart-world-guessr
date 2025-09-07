e2892bde4dc43baeff698e8e1e2e5aab448b53c0
Adrien Guéret <adrien.grt@gmail.com>
2025-09-07 17:07:01 +0200

    feat(home): add link to upload

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -37,8 +37,8 @@ function Home() {
             borderColor="#fb501e"
           />
         </li>
-        {/*
-         <li className="home-item__breakline" aria-hidden="true"></li>
+
+        <li className="home-item__breakline" aria-hidden="true"></li>
         <li className="home-item__upload">
           <Card
             icon={<HomeIcon icon="Upload" />}
@@ -47,7 +47,6 @@ function Home() {
             borderColor="#9e9e9eff"
           />
         </li>
-         */}
       </menu>
     </div>
   );
