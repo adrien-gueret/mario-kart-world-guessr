e11161730e7d89d39eaa3f4004daa43a26fa7dce
Adrien Guéret <adrien.grt@gmail.com>
2025-10-06 08:27:55 +0200

    fix(webhook): bind value instead of param

--- public/api/webhook.php
+++ public/api/webhook.php
@@ -48,7 +48,7 @@ if ($event['pull_request']['merged'] === true) {
         "INSERT INTO `mario-kart-world-notifications` (id_user, notification_type, specific_data)
         VALUES (:id_user, 'photo_validated', :specific_data)");
     $insertStmt->bindParam(':id_user', $photo['author_id'], PDO::PARAM_INT);
-    $insertStmt->bindParam(':specific_data', json_encode(['photo_id' => $photo['id']]), PDO::PARAM_STR);
+    $insertStmt->bindValue(':specific_data', json_encode(['photo_id' => $photo['id']]), PDO::PARAM_STR);
     $insertStmt->execute();
 
     http_response_code(200);
