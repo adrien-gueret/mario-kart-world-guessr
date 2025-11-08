4b7d61d0b488cb42975be51de1d44264e767ffbd
Adrien Guéret <adrien.grt@gmail.com>
2025-11-08 19:39:06 +0100

    feat(photos): show photos directly on the map

--- src/components/PhotosMap/index.tsx
+++ src/components/PhotosMap/index.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 import Map from "../Map";
 import Pin from "../Pin";
-import type { Photo } from "@/types/photos";
+import type { Photo as PhotoType } from "@/types/photos";
 
 type Props = {
-  photos: Photo[];
+  photos: PhotoType[];
 };
+
 export default function PhotosMap({ photos }: Props) {
+  const [zoomLevel, setZoomLevel] = useState(0);
+
+  const imageScaling = [1, 2, 3, 4];
+
   return (
     <div className="photo-details">
       <div
@@ -13,11 +20,20 @@ export default function PhotosMap({ photos }: Props) {
           position: "relative",
           textAlign: "center",
           width: "100%",
+          // @ts-ignore
+          "--image-scale": imageScaling[zoomLevel + 1] || 1,
         }}
       >
-        <Map shouldZoomOnDoubleClick>
+        <Map shouldZoomOnDoubleClick onZoomChange={setZoomLevel}>
           {photos.map((photo) => (
-            <Pin key={photo.id} x={photo.x} y={photo.y} />
+            <Pin
+              key={photo.id}
+              x={photo.x}
+              y={photo.y}
+              onlyHead
+              variant="image"
+              imageUrl={photo.photoUrl}
+            />
           ))}
         </Map>
       </div>
