c887e36e3d3630d830f26136d2ce2986f690e67e
Adrien Guéret <adrien.grt@gmail.com>
2025-11-30 11:56:41 +0100

    feat(albums): generate cover on update

--- public/api/my-albums.php
+++ public/api/my-albums.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/___middleware.php';
 
 require_once __DIR__ . '/___github.php';
 
+require_once __DIR__ . '/___album-cover.php';
+
 allowMethod('GET');
 
 if (empty($currentUser)) {
@@ -14,8 +16,7 @@ if (empty($currentUser)) {
 
 try {
     $stmt = $pdo->prepare(
-        "SELECT a.id, a.album_name as `name`,
-         CONCAT('https://www.mariouniversalis.fr/mario-kart-world-guessr/api/album-cover.php?id=', a.id) AS coverUrl
+        "SELECT a.id, a.album_name as `name`
         FROM `mario-kart-world-albums` a
         WHERE a.author_id = :authorId
         ORDER BY a.created_at DESC"
@@ -24,7 +25,12 @@ try {
     $stmt->bindParam(':authorId', $currentUser['id'], PDO::PARAM_INT);
     $stmt->execute();
 
-    $albums = $stmt->fetchAll(PDO::FETCH_ASSOC);  
+    $albums = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+    foreach ($albums as &$album) {
+        $album['coverUrl'] = getPublicAlbumCoverFromDisk((int)$album['id']);
+    }
 
     echo json_encode($albums);
 
