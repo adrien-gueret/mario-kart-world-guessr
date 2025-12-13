92784d0664e02a6cdf2dedc7b6206c65688d8b93
Adrien Guéret <adrien.grt@gmail.com>
2025-12-13 10:39:08 +0100

    feat(album): allow admin to see unpublished albums

--- public/api/___album.php
+++ public/api/___album.php
@@ -10,7 +10,7 @@ function getAlbumById(PDO $pdo, string $albumId, int $currentUserId = 0): ?array
             u.username AS author_name, u.locale, u.mario_character
         FROM `mario-kart-world-albums` AS a
         LEFT JOIN `mario-kart-world-users` AS u ON a.author_id = u.id
-        WHERE a.id = :albumId AND (a.author_id = :authorId OR a.is_published = 1)");
+        WHERE a.id = :albumId AND (a.author_id = :authorId OR a.is_published = 1 OR a.author_id = 1)");
 
     $stmt->bindValue(':albumId', $albumId, PDO::PARAM_INT);
     $stmt->bindValue(':authorId', $currentUserId, PDO::PARAM_INT);
