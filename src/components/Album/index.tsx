8aa3aab1f1ebbc87a38cdc10e75c381dbbe1c190
Adrien Guéret <adrien.grt@gmail.com>
2025-12-23 08:56:41 +0100

    fix(album,edit): do not render form layout if current user is not the author

--- src/components/Album/index.tsx
+++ src/components/Album/index.tsx
@@ -1,3 +1,4 @@
+import { Navigate } from "react-router-dom";
 import { useCurrentUser } from "@/auth/CurrentUserProvider";
 import type { Album } from "@/types/photos";
 
@@ -11,13 +12,15 @@ type Props = Album & {
 
 export default function Album({ isEditing = false, ...album }: Props) {
   const { user } = useCurrentUser();
+  const isCurrentUserTheAuthor = user?.id === album.author.id;
+
+  if (isEditing && !isCurrentUserTheAuthor) {
+    return <Navigate to={"/albums/" + album.id} />;
+  }
 
   return isEditing ? (
     <AlbumEdit {...album} />
   ) : (
-    <AlbumReadOnly
-      {...album}
-      isCurrentUserTheAuthor={user?.id === album.author.id}
-    />
+    <AlbumReadOnly {...album} isCurrentUserTheAuthor={isCurrentUserTheAuthor} />
   );
 }
